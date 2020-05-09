caa07e27666955c33fb0add09bf9a005c71f7166
Adam Pan <adampan94@gmail.com>
2020-05-08 17:47:30 -0700

    Added client side portion

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>MERN APP TEMPLATE</h1>
+    </Fragment>
+  );
+}
+export default App;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ connectDB();
 
 app.get('/', (req, res) => {res.send('API Running...')});
 
+//Define routes here
+app.use('/api/test', require('./routes/api/test'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
